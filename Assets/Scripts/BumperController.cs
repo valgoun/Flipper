00857fa50490408fd0b4d35fc0a0ff611bfd80fc
MONNET Thibaut <t.monnet@rubika-edu.com>
2017-01-19 16:05:46 +0100

    + intégration modele bumper

--- Assets/Scripts/BumperController.cs
+++ Assets/Scripts/BumperController.cs
@@ -1,18 +1,43 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using DG.Tweening;
 
 public class BumperController : MonoBehaviour {
 
     public GameObject Player;
     public float scoreValue;
     public float forceNeeded;
+    public float pikeSpeed;
+    public float wheelSpeed;
+
+    SkinnedMeshRenderer _anim;
+    Transform _wheel;
+
+    void Start()
+    {
+        if(gameObject.tag == "Bumper")
+        {
+            _anim = gameObject.GetComponentInChildren<SkinnedMeshRenderer>();
+            _wheel = transform.GetChild(0);
+        }
+    }
     
     void OnCollisionEnter(Collision other)
     {
         if(other.transform.tag == "Player" && other.relativeVelocity.magnitude > forceNeeded)
         {
-            //score
+            if (_anim != null)
+            {
+                DOTween.To(() => _anim.GetBlendShapeWeight(0), (x) => _anim.SetBlendShapeWeight(0, x), 100, pikeSpeed).OnComplete(() =>
+                 {
+                     DOTween.To(() => _anim.GetBlendShapeWeight(0), (x) => _anim.SetBlendShapeWeight(0, x), 0, pikeSpeed+2);
+                 });
+
+                _wheel.DORotate(new Vector3(0, 0, 360f), wheelSpeed, RotateMode.LocalAxisAdd).SetEase(Ease.InOutCubic);
+            }
+
+
             Debug.Log("bump");
         }
     } 
--- Assets/Scripts/FlipperController.cs
+++ Assets/Scripts/FlipperController.cs
@@ -3,7 +3,8 @@ using System.Collections.Generic;
 using UnityEngine;
 using DG.Tweening;
 
-public class FlipperController : MonoBehaviour {
+public class FlipperController : MonoBehaviour
+{
 
     public GameObject flipperL;
     public GameObject flipperR;
@@ -12,37 +13,72 @@ public class FlipperController : MonoBehaviour {
     public float maxAngle;
     public float minAngle;
     public float force;
+    public bool inverse;
 
     Rigidbody _rbL;
     Rigidbody _rbR;
 
 
-	// Use this for initialization
-	void Start () {
+    // Use this for initialization
+    void Start()
+    {
+        inverse = false;
         _rbL = axisL.GetComponent<Rigidbody>();
         _rbR = axisR.GetComponent<Rigidbody>();
     }
-	
-	// Update is called once per frame
-	void Update () {
-		if(Input.GetButton("Left"))
-        {
-            _rbL.DORotate(new Vector3(0, maxAngle, 0), 1f / force).OnComplete(() => _rbL.DORotate(new Vector3(0, minAngle, 0), 1f / force));
-        }
-        
-        if (Input.GetButtonUp("Left"))
-        {
-            _rbL.DORotate(new Vector3(0, minAngle, 0), 1f / force);
-        }
 
-        if (Input.GetButton("Right"))
+    // Update is called once per frame
+    void Update()
+    {
+
+        if (!inverse)
         {
-            _rbR.DORotate(new Vector3(0, -maxAngle + 180, 0), 1f / force).OnComplete(() => _rbR.DORotate(new Vector3(0, -minAngle + 180, 0), 1f / force));
+
+            if (Input.GetButton("Left"))
+                LeftFlipperUp();
+
+            if (Input.GetButtonUp("Left"))
+                LeftFlipperDown();
+
+            if (Input.GetButton("Right"))
+                RightFlipperUp();
+
+            if (Input.GetButtonUp("Right"))
+                RightFlipperDown();
         }
-        
-        if (Input.GetButtonUp("Right"))
+        else
         {
-            _rbR.DORotate(new Vector3(0, -minAngle + 180, 0), 1f / force);
+            if (Input.GetButton("Left"))
+                RightFlipperUp();
+
+            if (Input.GetButtonUp("Left"))
+                RightFlipperDown();
+
+            if (Input.GetButton("Right"))
+                LeftFlipperUp();
+
+            if (Input.GetButtonUp("Right"))
+                LeftFlipperDown();
         }
+    }
+
+    void LeftFlipperUp()
+    {
+        _rbL.DORotate(new Vector3(0, maxAngle, 0), 1f / force).OnComplete(() => _rbL.DORotate(new Vector3(0, minAngle, 0), 1f / force));
+    }
+
+    void LeftFlipperDown()
+    {
+        _rbL.DORotate(new Vector3(0, minAngle, 0), 1f / force);
+    }
+
+    void RightFlipperUp()
+    {
+        _rbR.DORotate(new Vector3(0, -maxAngle + 180, 0), 1f / force).OnComplete(() => _rbR.DORotate(new Vector3(0, -minAngle + 180, 0), 1f / force));
+    }
+
+    void RightFlipperDown()
+    {
+        _rbR.DORotate(new Vector3(0, -minAngle + 180, 0), 1f / force);
     }
 }
