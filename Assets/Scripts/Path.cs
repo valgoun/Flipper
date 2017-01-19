581b1f522f6e2e97f18e060b2e10d170dda9a07e
Keryann Malacchina <keryannm@gmail.com>
2017-01-19 15:46:00 +0100

    Path

--- Assets/Scripts/Path.cs
+++ Assets/Scripts/Path.cs
@@ -5,37 +5,25 @@ using DG.Tweening;
 
 public class Path : MonoBehaviour 
 {
-	[Header ("Editor")]
-	public bool launchPath;
-	public bool launchPathBackwards;
-	
 	[Header ("Ball")]
-	public Transform currentTarget;
 	public string ballTag;
-	public float ballVelocity;
+	public float ballVelocityFactor = 1;
 
 	[Header ("Path Points")]
 	public Vector3[] pathPoints = new Vector3[0];
 
-	[Header ("Path ")]
-	[Range (0, 1f)]
-	public float pathPercentage;
-
 	[Header ("Path Settings")]
+	public Ease pathEase = Ease.Linear;
 	public int pathResolution = 10;
 	public Color pathColor = Color.green;
 
 	private PathType pathType = PathType.CatmullRom;
 	private PathMode pathMode = PathMode.Full3D;
 
-	private Tween pathTween;
-
 	// Use this for initialization
 	void Start () 
 	{
 		SetupPath ();
-
-		//LaunchPath ();
 	}
 
 	void SetupPath ()
@@ -50,60 +38,23 @@ public class Path : MonoBehaviour
 		pathPoints [pathPoints.Length - 1] = transform.GetChild (transform.childCount - 1).position;
 	}
 
-	void LaunchPath (bool backwards = false)
+	void LaunchPath (Transform target, float velocity, bool backwards = false)
 	{
-		if (currentTarget == null)
-			currentTarget = GameObject.FindGameObjectWithTag (ballTag).transform;
-
-		if(!backwards)
-			pathTween = currentTarget.DOLocalPath (pathPoints, ballVelocity, pathType, pathMode, pathResolution, pathColor).SetSpeedBased ().SetEase (Ease.Linear).OnUpdate (PathPercentage);
-		else
-		{
-			pathTween = currentTarget.DOLocalPath (pathPoints, ballVelocity, pathType, pathMode, pathResolution, pathColor).SetSpeedBased ().SetEase (Ease.Linear).OnUpdate (PathPercentage);
-			pathTween.PlayBackwards ();
-		}
+		target.DOLocalPath (pathPoints, velocity * ballVelocityFactor, pathType, pathMode, pathResolution, pathColor).SetSpeedBased ().SetEase (pathEase).OnComplete (()=> PathComplete(target));
 	}
 
-	void GetPathEnd ()
+	void PathComplete (Transform target) 
 	{
-		
-	}
-
-	// Update is called once per frame
-	void Update () 
-	{
-		if(launchPath)
-		{
-			launchPath = false;
-
-			if (!pathTween.IsPlaying ())
-				LaunchPath ();
-			else
-				pathTween.PlayForward ();
-		}
-
-		if(launchPathBackwards)
-		{
-			launchPathBackwards = false;
-
-			if (pathTween.IsPlaying ())
-				pathTween.PlayBackwards ();
-		}
-	}
-
-	void PathPercentage ()
-	{
-		pathPercentage = pathTween.ElapsedPercentage ();
+		target.GetComponent<Rigidbody> ().velocity = Vector3.zero;
 	}
 
 	void OnTriggerEnter (Collider collider)
 	{
 		if(collider.tag == ballTag)
 		{
-			currentTarget = collider.transform;
-			ballVelocity = currentTarget.GetComponent<Rigidbody> ().velocity.magnitude;
+			float velocity = collider.transform.GetComponent<Rigidbody> ().velocity.magnitude;
 
-			LaunchPath ();
+			LaunchPath (collider.transform, velocity);
 		}
 	}
 }
