1656b79ab4d4b7736e8583086f110f7074979d3e
MINARDI Morgan <m.minardi@rubika-edu.com>
2017-01-19 15:23:15 +0100

    Création du PlayersManager et de la classe Player

---
+++ Assets/Player.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player {
+
+    private int ID;
+    private int score;
+
+
+    public Player(int id, int s)
+    {
+        this.ID = id;
+        this.score = s;
+    }
+
+
+    public int getID()
+    {
+        return this.ID;
+    }
+
+    public int getScore()
+    {
+        return this.score;
+    }
+
+}
---
+++ Assets/PlayersManager.cs
@@ -0,0 +1,70 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayersManager : MonoBehaviour {
+
+    public int credits;
+    public int numberOfPlayers;
+    public List<Player> Players;
+    public Player actualPlayer;
+    public int nexPlayerID;
+
+	// Use this for initializhation
+	void Start () {
+
+        credits = 0;
+        numberOfPlayers = 0;
+        Players = new List<Player>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+        if (Input.GetKeyDown("space"))
+        {
+            credits++;
+        }
+
+        if (Input.GetKeyDown(KeyCode.KeypadPlus))
+        {
+            if(credits > 0)
+            { 
+                numberOfPlayers++;
+                Players.Add(new Player(numberOfPlayers, 0));
+                actualPlayer = Players[0];
+                credits--;
+            }
+            else
+            {
+                Debug.Log("Insert coin");
+            }
+        }
+
+        if (Input.GetKeyDown("left"))
+        {
+            SwitchPlayer();
+        }
+
+    }
+
+    void SwitchPlayer()
+    {
+        nexPlayerID = actualPlayer.getID();
+
+        if(actualPlayer.getID() < numberOfPlayers)
+        {
+            Debug.Log(actualPlayer.getID());
+            actualPlayer = Players[nexPlayerID];
+        }
+        else if(actualPlayer.getID() == numberOfPlayers && credits > 0)
+        {
+            actualPlayer = Players[0];
+            credits--;
+        }
+        else
+        {
+            Debug.Log("Game's over !");
+        }
+    }
+}
