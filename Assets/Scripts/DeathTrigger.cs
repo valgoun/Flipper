e05a745836d21c1f0bf1e14ba9d8e5b15b501287
MONNET Thibaut <t.monnet@rubika-edu.com>
2017-01-19 12:13:24 +0100

    + table basique + flippers fonctionnels + tweak masses, frictions, etc

---
+++ Assets/Scripts/DeathTrigger.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using DG.Tweening;
+
+public class DeathTrigger : MonoBehaviour {
+
+    public float deathDelay;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	void OnTriggerEnter(Collider other)
+    {
+        if(other.tag == "Player")
+        {
+            DOVirtual.DelayedCall(deathDelay, () => {
+                other.transform.position = new Vector3(-1f, 1.65f, 0f);
+                other.gameObject.GetComponent<Rigidbody>().velocity = Vector3.zero;
+            });
+                
+        }
+
+    }
+}
---
+++ Assets/Scripts/FlipperController.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using DG.Tweening;
+
+public class FlipperController : MonoBehaviour {
+
+    public GameObject flipperL;
+    public GameObject flipperR;
+    public Transform axisL;
+    public Transform axisR;
+    public float maxAngle;
+    public float minAngle;
+    public float force;
+
+    Rigidbody _rbL;
+    Rigidbody _rbR;
+
+
+	// Use this for initialization
+	void Start () {
+        _rbL = axisL.GetComponent<Rigidbody>();
+        _rbR = axisR.GetComponent<Rigidbody>();
+    }
+	
+	// Update is called once per frame
+	void Update () {
+		if(Input.GetButton("Left"))
+        {
+            _rbL.DORotate(new Vector3(0, maxAngle, 0), 1f / force).OnComplete(() => _rbL.DORotate(new Vector3(0, minAngle, 0), 1f / force));
+        }
+
+        if (Input.GetButton("Right"))
+        {
+            _rbR.DORotate(new Vector3(0, -maxAngle + 180, 0), 1f / force).OnComplete(() => _rbR.DORotate(new Vector3(0, -minAngle + 180, 0), 1f / force));
+        }
+
+
+	}
+}
