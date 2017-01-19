5c2ef57a87ff99a3f02f3e47318a86c1a51d2b6c
ETU\e.escher <e.escher@rubika-edu.com>
2017-01-19 16:31:36 +0100

    Commit Eric Multiplicateurs et tunnels

---
+++ Assets/Script/InterruptTunnel.cs
@@ -0,0 +1,78 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class InterruptTunnel : MonoBehaviour {
+
+    public bool isInterruptBase;
+    public Transform tunnelLier;
+    private Tunnel tunnelLierScript;
+    private bool isActif;
+    private List<Tunnel> tunnelUpdate;
+    private GameObject[] tunnelRecup;
+    private List<InterruptTunnel> listInterrupt;
+    private GameObject[] findInterrupt;
+
+    private void Awake()
+    {
+        tunnelLierScript = tunnelLier.GetComponent<Tunnel>();
+        tunnelUpdate = new List<Tunnel>();
+        if (isInterruptBase)
+            isActif = true;
+        else
+            isActif = false;
+        tunnelRecup = GameObject.FindGameObjectsWithTag("trou");
+        for(int i = 0; i < tunnelRecup.Length; i++)
+        {
+            if(!tunnelRecup[i].transform.Equals(tunnelLier))
+                tunnelUpdate.Add(tunnelRecup[i].GetComponent<Tunnel>());
+        }
+        listInterrupt = new List<InterruptTunnel>();
+        findInterrupt = GameObject.FindGameObjectsWithTag("interruptTrou");
+        for(int i = 0; i < findInterrupt.Length; i++)
+        {
+            listInterrupt.Add(findInterrupt[i].GetComponent<InterruptTunnel>());
+        }
+
+    }
+
+    // Use this for initialization
+    void Start () {
+        if (isActif)
+        {
+            for(int i = 0; i < tunnelUpdate.Count; i++)
+            {
+                tunnelUpdate[i].Actif();
+                tunnelUpdate[i].Arriver(tunnelLier);
+            }
+            tunnelLierScript.Desactif();
+        }
+	}
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (collision.gameObject.tag.Equals("ball"))
+        {
+            isActif = !isActif;
+            if (isActif)
+            {
+                for (int i = 0; i < listInterrupt.Count; i++)
+                {
+                    listInterrupt[i].Desactif();
+                }
+                    for (int i = 0; i < tunnelUpdate.Count; i++)
+                {
+                    tunnelUpdate[i].Actif();
+                    tunnelUpdate[i].Arriver(tunnelLier);
+                }
+                tunnelLierScript.Desactif();
+            }
+        }
+    }
+
+    public void Desactif()
+    {
+        isActif = false;
+    }
+
+}
